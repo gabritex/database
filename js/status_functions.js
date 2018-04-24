7ab7f050fdcf632ab04c938c3ddd6e335f518839
gabritex <status-code@outlook.com>
2018-04-24 22:36:36 +0100

    Create status_functions.js

---
+++ js/status_functions.js
@@ -0,0 +1,25 @@
+  <script>
+			function myFunction0() {
+    			var myWindow = window.open("REGISTAR%20CLIENTE%20E%20EQUIPAMENTO.php", "_blank","toolbar=yes,scrollbars=yes,resizable=yes,top=40,left=850,width=500,height=550");
+			}				
+
+			function myFunction1() {
+    			var myWindow = window.open("MODIFICAR%20CLIENTE.php", "_blank","toolbar=yes,scrollbars=yes,resizable=yes,top=40,left=850,width=500,height=550");
+			}				
+	
+			function myFunction2() {
+    			var myWindow = window.open("ELIMINAR%20CLIENTE.PHP", "_blank","toolbar=yes,scrollbars=yes,resizable=yes,top=0,left=850,width=500,height=300");
+			}				
+
+			function myFunction3() {
+    			var myWindow = window.open("REGISTAR%20EQUIPAMENTO.php", "_blank","toolbar=yes,scrollbars=yes,resizable=yes,top=40,left=850,width=500,height=550");
+			}				
+
+			function myFunction4() {
+    			var myWindow = window.open("MODIFICAR%20EQUIPAMENTO.php", "_blank","toolbar=yes,scrollbars=yes,resizable=yes,top=40,left=850,width=500,height=550");
+			}				
+
+			function myFunction5() {
+    			var myWindow = window.open("ELIMINAR%20EQUIPAMENTO.PHP", "_blank","toolbar=yes,scrollbars=yes,resizable=yes,top=40,left=850,width=500,height=300");
+			}				
+		</script>
