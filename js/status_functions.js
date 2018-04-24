a83f5e11fc587964217ce0ce4c9e77708352451b
gabritex <status-code@outlook.com>
2018-04-24 22:37:29 +0100

    Update status_functions.js

--- js/status_functions.js
+++ js/status_functions.js
@@ -1,4 +1,3 @@
-  <script>
 			function myFunction0() {
     			var myWindow = window.open("REGISTAR%20CLIENTE%20E%20EQUIPAMENTO.php", "_blank","toolbar=yes,scrollbars=yes,resizable=yes,top=40,left=850,width=500,height=550");
 			}				
@@ -21,5 +20,4 @@
 
 			function myFunction5() {
     			var myWindow = window.open("ELIMINAR%20EQUIPAMENTO.PHP", "_blank","toolbar=yes,scrollbars=yes,resizable=yes,top=40,left=850,width=500,height=300");
-			}				
-		</script>
+			}
