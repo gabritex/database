db4d4239703ceae39d7ab30f61f82c66a98d3491
gabritex <Zero@MBP-de-Jose.lan>
2018-04-25 20:26:18 +0100

    _

--- _busca.php
+++ _busca.php
@@ -1,13 +1,13 @@
 <?php
 	//Incluir a conexão com banco de dados
-	include_once('conexao.php');
+	$con=mysqli_connect("localhost","root","","database3");
 	
 	//Recuperar o valor da palavra
 	$cursos = $_POST['palavra'];
 	
 	//Pesquisar no banco de dados nome do curso referente a palavra digitada pelo usuário
 	$cursos = "SELECT * FROM suites WHERE nome LIKE '$cursos%'";
-	$resultado_cursos = mysqli_query($conn, $cursos);
+	$resultado_cursos = mysqli_query($con, $cursos);
 	
 	if(mysqli_num_rows($resultado_cursos) <= 0){
 		echo "Nenhum curso encontrado...";
--- status_index.php
+++ status_index.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
 
-  <title>The HTML5 Herald</title>
+  <title>The HTML5 OSM</title>
   <meta name="OSM" content="Order Service Manager">
   <meta name="José Teixeira, Sergio" content="SitePoint v2">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -23,8 +23,9 @@
 </head>
 
 <body>
-<div class="container-fluid">  
-<form id="contact" action="inserir2.php" method="post">
+<div class="container-fluid"> 
+ 
+<form id="contact" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
   <div class="row">
     <div class="col">
     	<h3>Registo de Clientes</h3>
@@ -33,35 +34,74 @@
 		    <input placeholder="Telefone" type="text"  name="telefone">
 		    <input placeholder="Email" type="text"  name="email">
 		    <input placeholder="Morada" type="text" name="morada">
+		    <input placeholder="Contribuinte" type="text" name="contribuinte">
 		</fieldset>
 
 		<h3>Registo de Equipamento</h3>
-		<fieldset> 
-		    <input placeholder="Entrada" type="date"  name="entrada">
+		<fieldset>
 		    <input placeholder="Marca" type="text"  name="marca">
 		    <input placeholder="Modelo" type="text"  name="modelo">
 		    <input placeholder="Operadora" type="text"  name="operadora">
-		    <input placeholder="Serial/Emai" type="text"  name="seria">
-		    <input placeholder="Código de desbloqueio" type="text"  name="desbloqueio">
-		    <input placeholder="Acessórios" type="text"  name="acessorios">
-		    <input placeholder="Reclamação" type="text"  name="reclamacao">
-		    <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Registar</button>
+		    <input placeholder="Serial/EMAI" type="text"  name="serial">
 		</fieldset>
     </div>
     <div class="col">
     	<h3>Observações</h3>
 		<fieldset>
+			<input placeholder="Data_first" type="text"  name="entrada">
+			<input placeholder="Código de desbloqueio" type="text"  name="desbloqueio">
+		    <input placeholder="Acessórios" type="text"  name="acessorios">
+		    <input placeholder="Reclamação" type="text"  name="reclamacao">
 			<textarea rows="4" cols="500">Obs,</textarea>
 		</fieldset>
     	<h3>Registo de Serviço</h3>
 		<fieldset>
 			<textarea rows="4" cols="500">Reg,</textarea>
 		</fieldset>
+		<button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Save</button>
 	</div>
   </div>
 </form>
 </div>
 
+
+<?php
+if(isset($_POST['submit']))
+{
+
+// INICIA LIGAÇÃO À BASE DE DADOS
+$con=mysqli_connect("localhost","root","","database3");
+
+// VERIFICA A LIGAÇÃO NÃO TEM ERROS
+if (mysqli_connect_errno())
+{
+    // CASO TENHA ERROS MOSTRA O ERRO DE LIGAÇÃO À BASE DE DADOS
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+// CASO TUDO ESTEJA OK INSERE DADOS NA BASE DE DADOS
+$sql="INSERT INTO suites (nome, telefone, email, morada) VALUES ('$_POST[nome]','$_POST[telefone]','$_POST[email]','$_POST[morada]')";
+
+// CASO ESTEJA TUDO OK ADICIONA OS DADOS, SENÃO MOSTRA O ERRO
+if (!mysqli_query($con,$sql))
+{
+    die('Error: ' . mysqli_error($con));
+}
+
+
+// CASO TUDO ESTEJA OK INSERE DADOS NA BASE DE DADOS
+$sql2="INSERT INTO suites2 (entrada, marca, modelo, operadora, seria, desbloqueio, acessorios, reclamacao, nome) VALUES ('$_POST[entrada]','$_POST[marca]','$_POST[modelo]','$_POST[operadora]','$_POST[seria]','$_POST[desbloqueio]','$_POST[acessorios]','$_POST[reclamacao]','$_POST[nome]')";
+
+// CASO ESTEJA TUDO OK ADICIONA OS DADOS, SENÃO MOSTRA O ERRO
+if (!mysqli_query($con,$sql2))
+{
+    die('Error: ' . mysqli_error($con));
+}
+
+mysqli_close($con);
+
+}
+?>
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
